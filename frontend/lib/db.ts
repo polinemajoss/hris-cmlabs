094db2c3866a8eb87d752a5c1b4d0f6300d1c40c
mochwildaniazizi <2241720171@student.polinema.ac.id>
2025-06-02 22:42:47 +0700

    Delete frontend directory

--- frontend/lib/db.ts
+++
@@ -1,8 +0,0 @@
-import mysql from "mysql2/promise";
-
-export const db = await mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "", // kosongkan kalau default XAMPP
-  database: "hris_cmlabs", // Ganti sesuai nama database kamu
-});
